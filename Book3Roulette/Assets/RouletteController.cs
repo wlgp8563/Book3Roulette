ac8236911fb5d3f5c2fbf09a5e09d41daab29873
wlgp8563 <wlgp8563@naver.com>
2022-12-26 15:03:10 +0900

    bookexample
    bookexample roulette

---
+++ Book3Roulette/Assets/RouletteController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    float rotSpeed = 0;  // 회전 속도  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 클릭하면 회전 속도를 설정한다
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.rotSpeed = 10;
+        }
+
+        // 회전 속도만큼 룰렛을 회전시킨다
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        // 룰렛을 감속시킨다(추가)
+        this.rotSpeed *= 0.96f;
+    }
+}
